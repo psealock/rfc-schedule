66298535e6fca35a2c55db40370e0b404d76d726
Paul Sealock <psealock@gmail.com>
2022-07-06 22:06:34 +1200

    save

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '5e22de9d8801ee45caf4');
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log( 'hello world' );
+const { render } = wp.element;
+
+const App = () => {
+	return (
+		<div>
+			<h2>Hello World</h2>
+		</div>
+	);
+};
+
+if ( document.getElementById( 'my-react-app' ) ) {
+	//check if element exists before rendering
+	render( <App />, document.getElementById( 'my-react-app' ) );
+}
