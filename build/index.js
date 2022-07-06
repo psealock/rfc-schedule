35dfa63ce2042d3b363c8eb34710da1f40b04d61
Paul Sealock <psealock@gmail.com>
2022-07-06 17:52:22 +1200

    initial commit

---
+++ build/index.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('hello world');
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
