418991510409fd5b2bb032865316d079bf30b199
Paul Sealock <psealock@gmail.com>
2022-07-06 17:58:50 +1200

    main file

---
+++ rfc-schedule.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: RFC-Schedule
+ * Plugin URI: https://github.com/psealock/rfc-schedule
+ * Description: Raglan football schedule
+ * Version: 1.0.0
+ * Author: psealock
+ * Requires at least: 5.8
+ * Requires PHP: 7.2
+ *
+ * @package RFCSchedule
+ */
+
+defined( 'ABSPATH' ) || exit;
+
