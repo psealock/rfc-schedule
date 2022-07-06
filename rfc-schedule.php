77ced006baa813df0aa95cabe26f58347d5d48b9
Paul Sealock <psealock@gmail.com>
2022-07-06 22:02:10 +1200

    main page

--- rfc-schedule.php
+++ rfc-schedule.php
@@ -13,3 +13,38 @@
 
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * Short code output.
+ */
+function renderapp() {
+	return '<div id="rfc-schedule-app"></div>';
+}
+
+/**
+ * Make the JS magic happen.
+ */
+function enq_react() {
+
+	$script_path       = '/build/index.js';
+	$script_asset_path = dirname( __FILE__ ) . '/build/index.asset.php';
+	$script_asset      = file_exists( $script_asset_path )
+		? require $script_asset_path
+		: array(
+			'dependencies' => array(),
+			'version'      => filemtime( $script_path ),
+		);
+	$script_url        = plugins_url( $script_path, __FILE__ );
+
+	wp_register_script(
+		'rfc-schedule',
+		$script_url,
+		$script_asset['dependencies'],
+		$script_asset['version'],
+		true
+	);
+
+	wp_enqueue_script( 'rfc-schedule' );
+}
+
+add_shortcode( 'rfc-schedule', 'renderapp' );
+add_action( 'wp_enqueue_scripts', 'enq_react' );
