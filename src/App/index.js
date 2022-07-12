176e87c18460eb1ee0f20c1dfd68e3c77af10eac
Paul Sealock <psealock@gmail.com>
2022-07-12 14:41:11 +1200

    some styles

--- src/App/index.js
+++ src/App/index.js
@@ -3,6 +3,7 @@ import Schedule from './schedule';
 export default function App( { data } ) {
 	return (
 		<>
+			<h2>Saturday, July 30th</h2>
 			<Schedule data={ data } />
 		</>
 	);
--- src/App/table.js
+++ src/App/table.js
@@ -1,24 +1,52 @@
+import moment from 'moment';
+
 const Table = ( { fixtures, title } ) => {
 	return (
 		<div>
-			<h2>{ title }</h2>
+			<h3>{ title }</h3>
 			<table>
 				<thead>
 					<tr>
-						<th>Home Team</th>
-						<th>Away Team</th>
-						<th>Location</th>
 						<th>Time</th>
+						<th>Home Team</th>
+						<th></th>
+						<th>Away Team</th>
 					</tr>
 				</thead>
 				<tbody>
 					{ fixtures.map( ( fixture ) => {
+						const date = moment( fixture.Date );
+						const teamStyles = {
+							display: 'flex',
+							'flex-direction': 'column',
+							'align-items': 'center',
+						};
 						return (
 							<tr key={ fixture.Id }>
-								<td>{ fixture.HomeTeamName }</td>
-								<td>{ fixture.AwayTeamName }</td>
-								<td>{ fixture.VenueName }</td>
-								<td>{ fixture.Date }</td>
+								<td>
+									<span>{ date.format( 'h:mm a' ) }</span>
+									<br />
+									<span>{ fixture.VenueName }</span>
+								</td>
+								<td style={ teamStyles }>
+									<img
+										height="60px"
+										width="60px"
+										alt={ fixture.AwayTeamName }
+										src={ fixture.AwayOrganisationLogo }
+									/>
+									<span>{ fixture.AwayTeamName }</span>
+								</td>
+								<td>vs</td>
+								<td style={ teamStyles }>
+									<img
+										height="30px"
+										width="30px"
+										alt={ fixture.HomeTeamName }
+										src={ fixture.HomeOrganisationLogo }
+									/>
+									<span>{ fixture.HomeTeamName }</span>
+								</td>
 							</tr>
 						);
 					} ) }
